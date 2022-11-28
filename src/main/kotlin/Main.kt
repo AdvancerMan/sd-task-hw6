afc3c37b8aee836732fa54b409bc85f90fa03ede
Kazakov Mihail <advancerman@gmail.com>
2022-11-29 00:56:56 +0300

    Implement `Main`

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,38 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import tokenizer.TokenizerException
+import tokenizer.TokenizerImpl
+import visitor.CalcVisitor
+import visitor.ParserVisitor
+import visitor.PrintVisitor
+import visitor.VisitorException
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    val reader = System.`in`.bufferedReader()
+
+    try {
+        val tokenizer = TokenizerImpl()
+        while (true) {
+            val currentCharacter = reader.read()
+            if (currentCharacter == -1) {
+                tokenizer.handleCharacter(null)
+                break
+            }
+
+            tokenizer.handleCharacter(currentCharacter.toChar())
+        }
+
+        val parserVisitor = ParserVisitor()
+        tokenizer.getResult().forEach { it.accept(parserVisitor) }
+
+        val printVisitor = PrintVisitor(System.out)
+        parserVisitor.getResult().forEach { it.accept(printVisitor) }
+        println()
+
+        val calcVisitor = CalcVisitor()
+        parserVisitor.getResult().forEach { it.accept(calcVisitor) }
+        println("Result is ${calcVisitor.getResult()}")
+    } catch (e: VisitorException) {
+        println("Visitor error occurred: ${e.message}")
+    } catch (e: TokenizerException) {
+        println("Tokenizer error occurred: ${e.message}")
+    }
 }
